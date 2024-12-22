cb3b7ab70c039ab5728bde4fde2ccf4bacf34598
enneract9 <vargunin.iv@gmail.com>
2024-12-22 21:13:38 +0500

    Initial Commit

---
+++ SwiftUIMarathon8/SwiftUIMarathon8App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarathon8App.swift
+//  SwiftUIMarathon8
+//
+//  Created by @_@ on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarathon8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
